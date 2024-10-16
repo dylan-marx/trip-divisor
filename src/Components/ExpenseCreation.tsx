b83c52d444e5a32f88639985b833fb9b1b64e096
Dylan <dylanmarx0404@gmail.com>
2024-10-16 09:23:21 +0200

    Added add expense button

--- src/Components/ExpenseCreation.tsx
+++ src/Components/ExpenseCreation.tsx
@@ -1,4 +1,4 @@
-import { FC, useState } from "react";
+import { ChangeEvent, FC, useState } from "react";
 import Account from "./Objects/Account";
 import Expense from "./Objects/Expense";
 import './Styling/ExpenseCreation.css'
@@ -9,7 +9,8 @@ interface ExpenseCreationProps {
 }
 
 const ExpenseCreation : FC<ExpenseCreationProps> = ({allAccounts, allExpenses}) => {
-
+    const [description, setDescription] = useState('');
+    const [addingExpense, setAddingExpense] = useState(false);
     const [expenses, setExpenses] = useState<Expense[]>(allExpenses);
 
     const handlePayementChange = (expenseIndex: number, paymentIndex: number, newPaymentValue: number) => {
@@ -18,11 +19,46 @@ const ExpenseCreation : FC<ExpenseCreationProps> = ({allAccounts, allExpenses}) 
         setExpenses(newExpenses);
     }
 
+    const handleInputChange  = (event: ChangeEvent<HTMLInputElement>) => {
+        setDescription(event.target.value);
+    }
+
+    const addExpense = () => {
+        if (description.trim() !== '') {
+            const newPayments = new Array(allAccounts.length).fill(0);
+            const newExpense = new Expense(newPayments, description);
+            const newExpenses = [...expenses];
+            newExpenses.push(newExpense);
+    
+            setExpenses(newExpenses);
+            setDescription('');
+            setAddingExpense(false);
+        }
+
+    }
+
     return (
         <div className="expense-creation-container">
+            <div>
+                <div>What expenses did you have?</div>
+                {
+                    (!addingExpense) && <button onClick={() => setAddingExpense(true)}>Add Expense</button>
+                }
+
+                {
+                    addingExpense && (
+                        <div>
+                            <input type="text" value={description} onChange={handleInputChange}></input>
+                            <button onClick={addExpense}>Add</button>
+                            <button onClick={() => setAddingExpense(false)}>Cancel</button>
+                        </div>
+                    )
+                }
+                
+            </div>
             {   
                 
-                allExpenses.map((expense, expenseIndex) => (
+                expenses.map((expense, expenseIndex) => (
                     <div key={expenseIndex} className="expense">
                         <div>{expense.description}</div>
                         {
