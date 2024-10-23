cc2938e4b1c8dd077b2a0bcfbe307dcb9e983e96
Dylan <dylanmarx0404@gmail.com>
2024-10-23 10:20:48 +0200

    Fixed files

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/trip-divisor/',
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+        },
+      },
+    },
+  },
+
 })
